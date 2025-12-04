ef2c9207c4fc45bbbc01f2f64ca016b7adefb7b2
rayzer96 <raymond.bortolot@gmail.com>
2025-12-04 15:27:54 -0500

    Update test.py

--- diffuser_test/test.py
+++ diffuser_test/test.py
@@ -9,5 +9,5 @@ pipe = StableDiffusionPipeline.from_pretrained(
 )
 pipe = pipe.to("cuda")
 prompt = "black and white long haired cat"
-image = pipe(prompt, guidance_scale=5, num_inference_steps=50).images[0]
+image = pipe(prompt, guidance_scale=5, num_inference_steps=100).images[0]
 image.save(os.path.abspath("./astronaut_riding_horse.png"))
